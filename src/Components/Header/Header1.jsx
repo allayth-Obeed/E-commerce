296c0839a189f710f315672eba148d414acf0bb5
Allayth Obeed <allayt7obeed@gmail.com>
2026-03-15 20:29:27 +0200

    had been added selected menu

--- src/Components/Header/Header1.jsx
+++ src/Components/Header/Header1.jsx
@@ -1,32 +1,35 @@
-import { useContext , useState } from "react";
+import { useContext, useState } from "react";
 import { ColorModeContext } from "../../theme";
 import { IconButton, Stack, Typography, useTheme } from "@mui/material";
-import { DarkModeOutlined, Facebook, Instagram, LightModeOutlined, Twitter } from "@mui/icons-material";
-import {Box} from "@mui/material";
+import {
+  DarkModeOutlined,
+  Facebook,
+  Instagram,
+  LightModeOutlined,
+  Twitter,
+} from "@mui/icons-material";
+import { Box } from "@mui/material";
 // selected menu
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
 
-const options = [
-  'AR',
-  'EN',
-];
+const options = ["AR", "EN"];
 export default function Header1() {
   const colorMode = useContext(ColorModeContext);
   const theme = useTheme();
 
   // selected menu
-    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [anchorEl, setAnchorEl] = useState(null);
   const [selectedIndex, setSelectedIndex] = useState(1);
   const open = Boolean(anchorEl);
   const handleClickListItem = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
-  function handleMenuItemClick(event , index) {
+  function handleMenuItemClick(event, index) {
     setSelectedIndex(index);
     setAnchorEl(null);
   }
@@ -35,124 +38,120 @@ export default function Header1() {
     setAnchorEl(null);
   }
   return (
-    <Box sx={{bgcolor:"#283445",}}>
+    <Box sx={{ bgcolor: "#283445" }}>
       <Stack direction={"row"} alignItems={"center"}>
-    <Typography sx={{
-      mr:2,
-      p:"3px 10px",
-      bgcolor:"#d23f57",
-      borderRadius:"12px",
-      fontSize:"10px",
-      fontWeight:"bold",
-      color:"#fff"
-    }}>
-      HOT
-    </Typography>
-    <Typography sx={{
-      fontSize:"12px",
-      fontWeight:"300",
-      color:"#fff"
-    }}>
-      Free Express Shipping
-    </Typography>
-      <Box flexGrow={"1"}/>
-      <div>
-        {theme.palette.mode === "light" ? (
-          <IconButton 
-          sx={{color: "white"}}
-            onClick={() => {
-              localStorage.setItem(
-                "mode",
-                theme.palette.mode === "dark" ? "light" : "dark"
-              );
-              colorMode.toggleColorMode();
-            }}
-            color="inherit"
-          >
-            <LightModeOutlined fontSize="small" />
-          </IconButton>
-        ) : (
-          <IconButton
-            onClick={() => {
-              localStorage.setItem(
-                "mode",
-                theme.palette.mode === "dark" ? "light" : "dark"
-              );
-              colorMode.toggleColorMode();
-            }}
-            color="inherit"
-          >
-            <DarkModeOutlined fontSize="small" />
-          </IconButton>
-        )}
-      </div>
-      <List
-        component="nav"
-        aria-label="Device settings"
-        sx={{ bgcolor: 'background.paper' }}
-      >
-        <ListItemButton
-          id="lock-button"
-          aria-haspopup="listbox"
-          aria-controls="lock-menu"
-          aria-label="when device is locked"
-          aria-expanded={open ? 'true' : undefined}
-          onClick={handleClickListItem}
+        <Typography
+          sx={{
+            mr: 2,
+            p: "3px 10px",
+            bgcolor: "#d23f57",
+            borderRadius: "12px",
+            fontSize: "10px",
+            fontWeight: "bold",
+            color: "#fff",
+          }}
         >
-          <ListItemText
-            primary="When device is locked"
-            secondary={options[selectedIndex]}
-          />
-        </ListItemButton>
-      </List>
-      <Menu
-        id="lock-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        slotProps={{
-          list: {
-            'aria-labelledby': 'lock-button',
-            role: 'listbox',
-          },
-        }}
-      >
-        {options.map((option, index) => (
-          <MenuItem
-            key={option}
-            disabled={index === 0}
-            selected={index === selectedIndex}
-            onClick={(event) => handleMenuItemClick(event, index)}
+          HOT
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: "12px",
+            fontWeight: "300",
+            color: "#fff",
+          }}
+        >
+          Free Express Shipping
+        </Typography>
+        <Box flexGrow={"1"} />
+        <div>
+          {theme.palette.mode === "light" ? (
+            <IconButton
+              sx={{ color: "white" }}
+              onClick={() => {
+                localStorage.setItem(
+                  "mode",
+                  theme.palette.mode === "dark" ? "light" : "dark",
+                );
+                colorMode.toggleColorMode();
+              }}
+              color="inherit"
+            >
+              <LightModeOutlined fontSize="small" />
+            </IconButton>
+          ) : (
+            <IconButton
+              onClick={() => {
+                localStorage.setItem(
+                  "mode",
+                  theme.palette.mode === "dark" ? "light" : "dark",
+                );
+                colorMode.toggleColorMode();
+              }}
+              color="inherit"
+            >
+              <DarkModeOutlined fontSize="small" />
+            </IconButton>
+          )}
+        </div>
+        <List component="nav" sx={{ bgcolor: "transparent", color: "#fff" }}>
+          <ListItemButton
+            id="lock-button"
+            aria-haspopup="listbox"
+            aria-controls="lock-menu"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClickListItem}
           >
-            {option}
-          </MenuItem>
-        ))}
-      </Menu>
+            <ListItemText primary={options[selectedIndex]} />
+          </ListItemButton>
+        </List>
+        <Menu
+          id="lock-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          slotProps={{
+            list: {
+              "aria-labelledby": "lock-button",
+              role: "listbox",
+            },
+          }}
+        >
+          {options.map((option, index) => (
+            <MenuItem
+              key={option}
+              disabled={false}
+              selected={index === selectedIndex}
+              onClick={(event) => handleMenuItemClick(event, index)}
+            >
+              {option}
+            </MenuItem>
+          ))}
+        </Menu>
 
-      <IconButton >
-      <Facebook 
-      sx={{
-        fontSize:"14px",
-        color:"#fff"
-      }}
-      />
-      </IconButton>
-      <IconButton >
-      <Instagram 
-      sx={{
-        fontSize:"14px",
-        color:"#fff"
-      }}
-      />
-      </IconButton>
-      <IconButton >
-      <Twitter 
-      sx={{
-        fontSize:"14px",
-        color:"#fff"
-      }}
-      />
-      </IconButton>
+        <IconButton>
+          <Facebook
+            sx={{
+              fontSize: "14px",
+              color: "#fff",
+            }}
+          />
+        </IconButton>
+        <IconButton>
+          <Instagram
+            sx={{
+              fontSize: "14px",
+              color: "#fff",
+            }}
+          />
+        </IconButton>
+        <IconButton>
+          <Twitter
+            sx={{
+              fontSize: "14px",
+              color: "#fff",
+            }}
+          />
+        </IconButton>
       </Stack>
     </Box>
   );
